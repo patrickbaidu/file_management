73f22a03ccc13af19becef920d702b2f7f622ef6
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-04-22 12:33:02 +0800

    In this commit, I added the writing of the files to the separate copies for the odd_numbers.txt file and the even_numbers.txt file. Now, its tiem to turn the code into OOP. I was trying the codes out for the first time to familiarize myself of these codes since I am unfamiliar on handling these, but the next program, I'd like to try to start classing out my functions and make it more organized through oop.

--- integer_file_reader/integer_file_reader.py
+++ integer_file_reader/integer_file_reader.py
@@ -11,12 +11,23 @@ for line in number_file:
 
 for number in number_list:
     if number % 2 == 0:
+        number = str(number)
         even_number_list.append(number)
     else:
+        number = str(number)
         odd_number_list.append(number)
 
 print(number_list)
 print(odd_number_list)
 print(even_number_list)
+
+odd_numbers = ", ".join(odd_number_list)
+even_numbers = ", ".join(even_number_list)
+
+odd_number_file = open("odd_numbers.txt", "w")
+even_number_file = open("even)numbers.txt", "w")
+
+odd_number_file.write(odd_numbers)    
+even_number_file.write(even_numbers)
 
 number_file.close()
