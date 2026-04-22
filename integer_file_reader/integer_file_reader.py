cfb9e2c8b659425bca655a2c7ed407dd5e6481dd
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-04-22 12:12:44 +0800

    In this commit, I open the file, then used the list array to contain the numbers then used the modulo operator to separater the odd numbers and even numbers to their own lists.

--- integer_file_reader/integer_file_reader.py
+++ integer_file_reader/integer_file_reader.py
@@ -1,8 +1,22 @@
-number_file = open("file_management/integer_file_reader/numbers.txt", "r")
+number_file = open("file_management/integer_file_reader/numbers.txt")
 
-file_read = number_file.read()
+number_list = []
+odd_number_list = []
+even_number_list = []
 
-for line in file_read:
-    print(line)
+
+for line in number_file:
+    line = int(line.strip())
+    number_list.append(line)
+
+for number in number_list:
+    if number % 2 == 0:
+        even_number_list.append(number)
+    else:
+        odd_number_list.append(number)
+
+print(number_list)
+print(odd_number_list)
+print(even_number_list)
 
 number_file.close()
