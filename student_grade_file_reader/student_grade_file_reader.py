dcb70290eef58e91140c15a12ce57458953f8849
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-04-26 11:23:37 +0800

    In this commit, I used the objects to open up my files and then pass it so that it will split the data of the text file to lists.

--- student_grade_file_reader/student_grade_file_reader.py
+++ student_grade_file_reader/student_grade_file_reader.py
@@ -2,3 +2,10 @@ from student_grade_file_reader_functions import OpenFile
 from student_grade_file_reader_functions import StudentFile
 from student_grade_file_reader_functions import HighestGrade
 from student_grade_file_reader_functions import TopStudent
+
+file_name = "file_management/student_grade_file_reader/students_grades.txt"
+
+try:
+    file = OpenFile(file_name)
+    opened_file = OpenFile.open_file(file)
+    student_file = StudentFile(opened_file)
