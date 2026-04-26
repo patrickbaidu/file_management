02d6c8be35e42b49730a3944b722be3b865d677f
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-04-26 17:32:43 +0800

    In this commit, I opened the file of integers.txt using the method open_file() from the openfile class.

--- power_of_integer_file_reader/power_of_integer_file_reader.py
+++ power_of_integer_file_reader/power_of_integer_file_reader.py
@@ -1,3 +1,8 @@
 from power_of_integer_functionalities import OpenFile
 from power_of_integer_functionalities import IntegerFunctions
 from power_of_integer_functionalities import WriteFile
+
+file_name = "file_management/power_of_integer_file_reader/integers.txt"
+
+file_name = OpenFile(file_name)
+opened_file = file_name.open_file()
