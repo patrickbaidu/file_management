b0d8ea5e895e97d6279c750cf039f356eabf4e88
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-04-26 09:22:58 +0800

    In this commit, this is the second project I am working on right now, it is the student grade computer where it gets the maximum grade of the student with the highest one. I need to determine the pairs for this one, and I could either use tuples or dictionaries for this one in indexing the students and the grades.

---
+++ student_grade_file_reader/student_grade_file_reader_functions.py
@@ -0,0 +1,10 @@
+class GradeFunctionalities:
+    
+    
+    def __init__(self, student_name, student_grade):
+        self.student_name = student_name
+        self.student_grade = student_grade
+        self.file_name = file_name
+    
+    def open_file(self):
+        number_file = open()
