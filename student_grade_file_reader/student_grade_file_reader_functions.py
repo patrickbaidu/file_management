2daaae7b2f964fa9234a041052acd92a185f3cab
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-04-26 09:25:42 +0800

    I added the object for opening the file.

--- student_grade_file_reader/student_grade_file_reader_functions.py
+++ student_grade_file_reader/student_grade_file_reader_functions.py
@@ -1,10 +1,14 @@
-class GradeFunctionalities:
+class OpenFile:
     
+    def __init__(self, file_name):
+        self.file_name = file_name
+    
+    def open_file(self):
+        number_file = open(self.file_name)
+        return number_file
+
+class GradeFunctionalities:
     
     def __init__(self, student_name, student_grade):
         self.student_name = student_name
         self.student_grade = student_grade
-        self.file_name = file_name
-    
-    def open_file(self):
-        number_file = open()
