cdbfd6de53f6074102fb50ab5c5841823b229481
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-04-26 17:13:02 +0800

    In this commit, I added the method to open the file for the integers.txt.

--- power_of_integer_file_reader/power_of_integer_functionalities.py
+++ power_of_integer_file_reader/power_of_integer_functionalities.py
@@ -1,7 +1,11 @@
 class OpenFile:
     
-    def __init__(self):
-        pass
+    def __init__(self, file_name):
+        self.file_name = file_name
+    
+    def open_file(self):
+        number_file = open(self.file_name)
+        return number_file
 
 class IntegerFunctions:
     
