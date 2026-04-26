d8a7b6aab55afa6a6d6b042917561226d4327d00
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-04-26 11:56:43 +0800

    In this commit I added the try and except block, then it was done for the second program.

--- student_grade_file_reader/student_grade_file_reader.py
+++ student_grade_file_reader/student_grade_file_reader.py
@@ -5,19 +5,23 @@ from student_grade_file_reader_functions import TopStudent
 
 file_name = "file_management/student_grade_file_reader/students_grades.txt"
 
-file = OpenFile(file_name)
-opened_file = OpenFile.open_file(file)
-student_file = StudentFile(opened_file)
+try:
+    file = OpenFile(file_name)
+    opened_file = OpenFile.open_file(file)
+    student_file = StudentFile(opened_file)
 
-student_data_list = student_file.split_data_file()
-student_name_list = student_file.add_student_name(student_data_list)
-student_grade_list = student_file.add_student_grade(student_data_list)
+    student_data_list = student_file.split_data_file()
+    student_name_list = student_file.add_student_name(student_data_list)
+    student_grade_list = student_file.add_student_grade(student_data_list)
 
-student_grades = HighestGrade(student_grade_list)
-highest_grade = student_grades.get_max_grade()
-highest_grade_index = student_grades.get_grade_index(highest_grade)
+    student_grades = HighestGrade(student_grade_list)
+    highest_grade = student_grades.get_max_grade()
+    highest_grade_index = student_grades.get_grade_index(highest_grade)
 
-student_names = TopStudent(student_name_list)
-top_student = student_names.get_top_student(highest_grade_index)
+    student_names = TopStudent(student_name_list)
+    top_student = student_names.get_top_student(highest_grade_index)
 
-print(top_student, highest_grade)
+    print(top_student, highest_grade)
+
+except:
+    print("Error in handling the request.")
