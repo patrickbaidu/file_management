c5badd038c18ee793d13bf2cae11bf4d1ce574a7
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-04-26 09:36:24 +0800

    I added the student class then I added the method to split the data file, because in the text file, the grades and the students where separated by an equal sign. So I appended the list of the data which was separated by the split function, then for every data in the data list, I append the first index as the name, and the second index to be the grade.

--- student_grade_file_reader/student_grade_file_reader_functions.py
+++ student_grade_file_reader/student_grade_file_reader_functions.py
@@ -4,11 +4,30 @@ class OpenFile:
         self.file_name = file_name
     
     def open_file(self):
-        number_file = open(self.file_name)
-        return number_file
+        grades_file = open(self.file_name)
+        return grades_file
 
+class Student:
+    
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = grade
+    
+    def split_data_file(self, grades_file):
+        student_name_list = []
+        student_grade_list = []
+        data_list = []
+        for data in grades_file:
+            data = data.replace(" = ", "")
+            data = data.split()
+            data_list.append(data)
+        for data in data_list:
+            student_name_list.append([0])
+            student_grade_list.append([1])
+        return student_grade_list and student_name_list
+    
 class GradeFunctionalities:
     
-    def __init__(self, student_name, student_grade):
-        self.student_name = student_name
+    def __init__(self, student_grade):
         self.student_grade = student_grade
+    
