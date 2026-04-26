9dbbec95c967348082f36412952e53b76cec3d55
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-04-26 12:59:32 +0800

    In this commit I added two methods, opening the file and appending the file.

--- text_file_encoder/text_file_encoder_functions.py
+++ text_file_encoder/text_file_encoder_functions.py
@@ -1,7 +1,15 @@
 class AppendFile:
     
-    def __init__(self):
-        pass
+    def __init__(self, file_name):
+        self.file_name = file_name
+    
+    def open_file(self):
+        text_file = open(self.file_name, "a")
+        return text_file
+    
+    def append_file(self, text_file, input):
+        text_file.write(input)
+
 class InputText:
     
     def __init__(self):
