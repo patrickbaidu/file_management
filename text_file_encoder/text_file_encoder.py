1994ee12a128dc48ee214a88a8c2f502c71224b9
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-04-26 13:08:12 +0800

    In this commit, I imported my classes, then used it to open up my file, my_life.txt.

---
+++ text_file_encoder/text_file_encoder.py
@@ -0,0 +1,8 @@
+from text_file_encoder_functions import FileHandling
+from text_file_encoder_functions import InputText
+
+file_name = "my_life.txt"
+
+file_name = FileHandling(file_name)
+make_file = file_name.open_file()
+
