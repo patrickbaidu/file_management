7ebb541f1135ddfbf4135ef72b2086c2583ecc57
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-04-26 08:37:19 +0800

    In this commit, I made an object for the open file and for the number functions.

--- integer_file_reader/integer_file_reader.py
+++ integer_file_reader/integer_file_reader.py
@@ -1,18 +1,3 @@
-from integer_functionalities import IntegerFunctionalities
+from integer_functionalities import OpenFile
+from integer_functionalities import NumberFunctionalities
 
-number_list = []
-file_name = "file_management/integer_file_reader/numbers.txt"
-
-number_file = IntegerFunctionalities.open_number_file(file_name)
-
-for line in number_file:
-    line = IntegerFunctionalities.make_number_integer(line)
-    number_list.append(line)
-
-for number in number_list:
-    even_numbers = IntegerFunctionalities.get_even(number)
-    odd_numbers = IntegerFunctionalities.get_odd(number)
-    even_numbers = ", ".join(even_numbers)
-    odd_numbers = ", ".join(odd_numbers)
-
-separate_file = IntegerFunctionalities.write_number_file(odd_numbers, even_numbers)
--- integer_file_reader/integer_functionalities.py
+++ integer_file_reader/integer_functionalities.py
@@ -1,37 +1,9 @@
-class IntegerFunctionalities:
+class OpenFile:
     
-    def __init__(self, oddnumber, evennumber, number, filename, numberfile, numberline):
-        self.oddnumber = oddnumber
-        self.evennumber = evennumber
-        self.number = number
-        self.filename = filename
-        self.numberfile = numberfile
-        self.numberline = numberline
+    def __init__(self):
+        pass
+
+class NumberFunctionalities:
     
-    def make_number_integer(self, numberline):
-        numberline = int(numberline.strip())
-    
-    def get_even(self, number):
-        even_number_list = []
-        if number % 2 == 0:
-            number = str(number)
-            even_number_list.append(number)
-        return even_number_list
-    
-    def get_odd(self, number):
-        odd_number_list = []
-        if number % 2 == 1:
-            number = str(number)
-            odd_number_list.append(number)
-        return odd_number_list
-    
-    def open_number_file(self, filename):
-        number_file = open(filename)
-        return number_file
-    
-    def write_number_file(self, oddnumber, evennumber):
-        odd_number_file = open("odd_numbers.txt", "w")
-        even_number_file = open("even_numbers.txt", "w")
-        
-        odd_number_file.write(oddnumber)
-        even_number_file.write(evennumber)
+    def __init__(self)
+
