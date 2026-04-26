c8fff803b22958d8a3fb971b51414bc6ddf6f9aa
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-04-26 09:18:47 +0800

    Finally, I added the try except block to handling the error request if an error pops up.

--- integer_file_reader/integer_file_reader.py
+++ integer_file_reader/integer_file_reader.py
@@ -4,16 +4,17 @@ from integer_functionalities import WriteFile
 
 file_name = "file_management/integer_file_reader/numbers.txt"
 
-file = OpenFile(file_name)
-opened_file = OpenFile.open_file(file)
-number_file = NumberFunctionalities(opened_file)
+try:
+    file = OpenFile(file_name)
+    opened_file = OpenFile.open_file(file)
+    number_file = NumberFunctionalities(opened_file)
 
-list_of_numbers = number_file.get_number()
-even_numbers_list = number_file.get_even_number(list_of_numbers)
-odd_numbers_list = number_file.get_odd_number(list_of_numbers)
+    list_of_numbers = number_file.get_number()
+    even_numbers_list = number_file.get_even_number(list_of_numbers)
+    odd_numbers_list = number_file.get_odd_number(list_of_numbers)
 
-write_number_file = WriteFile(even_numbers_list, odd_numbers_list)
-write_number_file.write_file()
+    write_number_file = WriteFile(even_numbers_list, odd_numbers_list)
+    write_number_file.write_file()
 
-print(even_numbers_list)
-print(odd_numbers_list)
+except:
+    print("Error in Handling the Request.")
