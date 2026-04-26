3b432c92eb36b298694f3c3807b45cc882f046a2
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-04-26 17:10:56 +0800

    In this commit, I added my class objects for the use.

---
+++ power_of_integer_file_reader/power_of_integer_functionalities.py
@@ -0,0 +1,14 @@
+class OpenFile:
+    
+    def __init__(self):
+        pass
+
+class IntegerFunctions:
+    
+    def __init__(self):
+        pass
+
+class WriteFile:
+    
+    def __init__(self):
+        pass
