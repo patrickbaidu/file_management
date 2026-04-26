943ca3c7ba7a5c9a6bc06cac27e793cd23eb0a07
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-04-26 17:47:42 +0800

    In this commit, I added a color class to my student grade file reader.

--- student_grade_file_reader/student_grade_file_reader_functions.py
+++ student_grade_file_reader/student_grade_file_reader_functions.py
@@ -55,3 +55,15 @@ class TopStudent:
     def get_top_student(self, index_grade):
         top_student = self.student_list[index_grade]
         return top_student
+
+class Color:
+    
+    purple = '\033[95m'
+    red = '\033[91m'
+    yellow = '\033[93m'
+    blue = '\033[94m'
+    cyan = '\033[96m'
+    bold = '\033[1m'
+    underline = '\033[4m'
+    end = '\033[0m'
+    green = '\033[92m'
